ee2d080c2690ce9f0a8b32fcd2881e8c0ed58749
jongbin7645 <132874186+jongbin7645@users.noreply.github.com>
2023-10-22 22:11:59 +0900

    Add files via upload

---
+++ js/layout.js
@@ -0,0 +1,16 @@
+$('document').ready(function(){
+
+
+    $('.banner').slick({
+        'nextArrow' : '.btn.type1',
+        'prevArrow' : ' .btn.type2',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+
+    
+    })
+    
+});
+
